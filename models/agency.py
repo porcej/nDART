218b98d7831bdf55b2a8923d519d6bcf576506ac
Joseph Porcelli <porcej@gmail.com>
2025-05-11 10:16:10 -0400

    Updated Agency Model Relationships

--- models/agency.py
+++ models/agency.py
@@ -2,11 +2,18 @@ from extensions import db
 from uuid import uuid4
 
 class Agency(db.Model):
+    """Model for agencies"""
+    __tablename__ = 'agencies'
+
+    # Columns   
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid4()))
     name = db.Column(db.String(100), nullable=False)
     display_name = db.Column(db.String(100), nullable=True)
     description = db.Column(db.String(200), nullable=True)
     enabled = db.Column(db.Boolean, default=True)
+
+    # Relationships
+    events = db.relationship('Event', backref=db.backref('agency', lazy=True))
 
     def __repr__(self):
         return f"<Agency {self.name}>"
